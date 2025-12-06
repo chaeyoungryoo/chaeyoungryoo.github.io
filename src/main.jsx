493aa79061b3124376ad097cc7a77f79f9a1f199
Chaeyoung <ryoochaeyoung@gmail.com>
2025-12-06 12:48:29 +0900

    Settings

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import '@assets/css/reset.css'
+import '@assets/css/common.css'
+import '@assets/css/font.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
