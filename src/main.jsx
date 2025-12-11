9994c683b6ba2c9fd4169df0226cc256e9f86ffc
Chaeyoung <ryoochaeyoung@gmail.com>
2025-12-11 13:17:57 +0900

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,5 +5,6 @@ import App from './App'
 import '@assets/css/reset.css'
 import '@assets/css/common.css'
 import '@assets/css/font.css'
+import '@assets/css/print.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
